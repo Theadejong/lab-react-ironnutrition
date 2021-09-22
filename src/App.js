12bcc499f7b187b0f75e59cd1c4d5bab12e7f9a0
Theadejong <theadejong85@gmail.com>
2021-09-22 16:18:59 +0200

    lab done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import foodsData from "./foods.json";
+import {useState} from 'react';
+import FoodBox from './component/FoodBox'
+import { Row } from 'antd';
+import AddFoodForm from './component/AddFoodForm';
+import SearchFood from './component/SearchFood';
 
 function App() {
+
+  const [foods, setFood] = useState(foodsData);
+  const [searchInput, setSearchInput] = useState('')
+  const [foodsCopy, setFoodsData] = useState(foodsData)
+
+  //function to add a food
+  const addNewFood = (newFood)=>{
+    const updatedFoodData = [...foods, newFood]
+    const updatedFoodCopy = [...foods, newFood]
+    setFood(updatedFoodData)
+    setFoodsData(updatedFoodCopy)
+  }
+
+  //function to filter 
+  const searchFoodFilter = (e) =>{
+    console.log(e)
+    setSearchInput(e.target.value)
+
+    if(e.target.value === ''){
+      setFood(foods)
+    }
+
+    const textInputValue = e.target.value.toLowerCase();
+
+    const filteredList = foodsCopy.filter((food)=>{
+      const foodIncludes = food.name.toLowerCase();
+      return foodIncludes.includes(textInputValue);
+
+    })
+    setFood(filteredList)
+    }
+
+  
+    const deleteFood = (name) =>{
+      const foodToDelete = foodsCopy.filter((food)=>{
+        return food.name.toLowerCase() !== name.toLowerCase()
+      })
+      setFood(foodToDelete)
+    }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      <AddFoodForm addFood={addNewFood}/>
+      <h2>Food List</h2>
+
+      <SearchFood searchInput={searchInput} searchFoodFilter={searchFoodFilter} />
+
+      <Row>
+      {foods.map(food => {
+        return (
+          <FoodBox food={food} key={food.name} deleteFood={deleteFood}/>
+        )}
+        )}
+      
+      </Row>
+    
+      
+      </div>
+  )
 }
 
 export default App;
